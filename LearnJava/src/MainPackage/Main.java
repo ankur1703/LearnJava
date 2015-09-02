aa2df8739334152caa3eacceb686ed59b610dd0c
Ankur Yadav <ankur7.9@outlook.com>
2015-09-02 17:09:41 +0100

    RotateArray

--- LearnJava/src/MainPackage/Main.java
+++ LearnJava/src/MainPackage/Main.java
@@ -4,12 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		char c = StringCodes.FindFirstNonRepeatingCharacter("stress");
+		//char c = StringCodes.FindFirstNonRepeatingCharacter("stress");
 		
-		System.out.println(c);
+		//System.out.println(c);
 		
+		int arr[] = {1, 2, 3, 4, 5, 6, 7};
 		
-
+		StringCodes.rotateArray(arr, 3);
+		//StringCodes.rotateArrayDivAndConq(arr, 3);
+		System.out.println();
 	}
 
 		
