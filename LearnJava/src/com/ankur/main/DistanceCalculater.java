db8c3ec74a572edb58411cd33aa5ae843d40e1e6
Ankur <ankur1703@yahoo.co.in>
2015-10-17 19:54:47 +0100

    Updated

--- LearnJava/src/com/ankur/main/DistanceCalculater.java
+++ LearnJava/src/com/ankur/main/DistanceCalculater.java
@@ -5,10 +5,17 @@ public class DistanceCalculater {
 	public double calculateDistance(double latitudeOfPoint1, double longitudeOfpoint1, double latitudeOfPoint2, double longitudeOfpoint2){
 		double distance = 0d;
 		int radius = 6371;
-		double differenceBetweenLongitude = Math.abs(longitudeOfpoint1-longitudeOfpoint2);
-		double angleBetweenPoints = Math.acos(Math.sin(latitudeOfPoint1)*Math.sin(latitudeOfPoint2) + Math.cos(latitudeOfPoint1)*Math.cos(latitudeOfPoint2)*differenceBetweenLongitude);
-		distance = radius*angleBetweenPoints* Math.PI/180;
 		
+		latitudeOfPoint1 = Math.toRadians(latitudeOfPoint1);
+		latitudeOfPoint2 = Math.toRadians(latitudeOfPoint2);	
+		double differenceBetweenLongitude = Math.toRadians(Math.abs(longitudeOfpoint1-longitudeOfpoint2));
+		
+		double angleBetweenPoints = Math.acos(Math.sin(latitudeOfPoint1)*Math.sin(latitudeOfPoint2)
+								              + Math.cos(latitudeOfPoint1)*Math.cos(latitudeOfPoint2)
+								              *Math.cos(differenceBetweenLongitude));
+		
+		distance = radius*angleBetweenPoints;
 		return distance;
 	}
+	
 }
